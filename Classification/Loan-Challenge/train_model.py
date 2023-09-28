a0114477c8b90c5de7e6865afb63f14d1f1fe6be
Xavirs <draculcodes@gmail.com>
2023-09-28 23:29:43 +0200

    Readme updated and GridSearchCV temporarily removed for Loan-Challenge

--- Classification/Loan-Challenge/train_model.py
+++ Classification/Loan-Challenge/train_model.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import make_scorer, roc_auc_score, classification_report
-
+from sklearn.metrics import precision_score, accuracy_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 data = pd.read_csv('balanced_training_data.csv')
 
@@ -11,33 +11,12 @@ y = data['loan_status']
 X = data.drop('loan_status', axis = 1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-#Find the best estimator for our RandomForest model:
-custom_scorer = make_scorer(roc_auc_score, greater_is_better=True)
-param_grid = {'n_estimators': [50, 100, 150, 200, 250, 300],  # being the number of trees in the forest.
-                  'min_samples_leaf': [3, 5, 10, 20],  # number of minimum samples required at a leaf node.
-                  'min_samples_split': [3, 6, 9],  # number of minimum samples required to split an internal node.
-                  'criterion': ['entropy'],  # measures the quality of a split. Can use gini's impurity or entropy.
-                  # 'subsample':[0.5,0.8,1]#buscar con mas detalle
-                  # 'reg_lambda':[1,10,100]#buscar con mas detalle
-                  }
-
-clf = GridSearchCV(
-# Evaluates the performance of different groups of parameters for a model based on cross-validation.
-    RandomForestClassifier(class_weight='balanced', bootstrap=False, random_state=1234),
-    param_grid,  # dict of parameters.
-    cv=10,  # Specified number of folds in the Cross-Validation(K-Fold).
-    scoring='f1_micro')
-
-clf.fit(X_train, y_train)
-
-print(clf.best_estimator_)
-model = clf.best_estimator_  # Estimator that was chosen by the search, i.e. estimator which gave highest score (or smallest loss if specified) on the left out data
+rf = RandomForestClassifier(class_weight='balanced', n_estimators=150, bootstrap=False)
 
 
-#Roc_Auc_Score was used because in the original challenge, it was the established metric.
 print('RandomForest:')
-y_pred = model.predict(X_test)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
 print(roc_auc_score(y_pred, y_test))
-print(classification_report(y_pred, y_test))
+print(precision_score(y_pred, y_test))
 print('-------------------------------')
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DataScienceChallenges
+
+This is a collection of all the challenges that I will take in order to learn and grasp all the concepts in the areas of Machine and Deep Learning. 
+
+## Classification Challenges:
+
+### Horses Health:
+https://github.com/XaviRS95/DataScienceChallenges/tree/main/Classification/Horses-Health
+
+Kaggle competition where you have to predict the outcome of a horse (lived, died or euthanized) using the provided dataset using f1-score. The URL for this challenge is: https://www.kaggle.com/competitions/playground-series-s3e22 
+
+So far, my highest punctuation has been of 0.78 using RandomForest. There are still some feature engineering and other techniques that I want to try, since other people have been able to obtain 0.85.
+
+When using classification_report, I have noticed that 1 category has a lower precision than the rest (around 0.1), while another has the same problem with the recall. I'm currently working on what is causing this imbalance and how I can solve it, since it might be the key to improve the f1-score to 0.8
